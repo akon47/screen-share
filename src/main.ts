ca3ec7b3ffa0c858d00e7f8535254c08edd0dac8
Kim, Hwan <akon47@naver.com>
2022-10-29 19:04:30 +0900

    webrtc 호환성을 위해 adapter를 추가합니다.

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './routes';
 import store from './store'
+import adapter from 'webrtc-adapter';
 
+console.log(adapter.browserDetails.browser);
 createApp(App).use(router).use(store).mount('#app')
